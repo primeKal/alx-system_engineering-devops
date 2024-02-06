aa6aaa50fef48ece99715ccdb1a592146841614d
Kaleb Teshale <kalebteshale72@gmail.com>
2024-02-06 15:42:13 +0300

    mysql replication

---
+++ 0x14-mysql/helpers/steps_replica.sql
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#Steps to establish replication process between servers
+replica_server_ip=
+source_server_ip=
+sudo ufw allow from $replica_server_ip to any port 3306
+sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
+bind-address = $source_server_ip
+#uncomment server-id = 1
+#uncomment log_bin = /var/log/mysql/mysql-bin.log
